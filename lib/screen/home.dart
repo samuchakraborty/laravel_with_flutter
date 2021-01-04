a857c1c2d57ed6f9f5cbf536b402faaebcef97dc
samuchakraborty <samuckb@gmail.com>
2021-01-04 17:12:37 +0600

    successfull with error

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -4,72 +4,85 @@ import 'package:login_register_with_laravel/network_utils/api.dart';
 import 'package:login_register_with_laravel/screen/login.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   String name;
+  String token;
   @override
-  void initState(){
+  void initState() {
     _loadUserData();
     super.initState();
   }
-  _loadUserData() async{
+
+  _loadUserData() async {
     SharedPreferences localStorage = await SharedPreferences.getInstance();
     var user = jsonDecode(localStorage.getString('data'));
 
-    if(user != null) {
+    if (user != null) {
       setState(() {
         name = user['name'];
+        token = user['token'];
       });
     }
   }
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Test App'),
-            backgroundColor: Colors.teal,
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Hi, $name',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Center(
-                    child: RaisedButton(
-                      elevation: 10,
-                      onPressed: (){
-                        logout();
-                      },
-                      color: Colors.teal,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: Text('Logout'),
-                    ),
-                  ),
-                ],
-              ),
-          ),
-        );
-      }
 
-  void logout() async{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test App'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Hi, $name',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Hi, $token',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 4),
+            ),
+            RaisedButton(
+              elevation: 10,
+              onPressed: () {
+                logout();
+                print("on pressed");
+              },
+              color: Colors.teal,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void logout() async {
     var res = await Network().getData('/logout');
     var body = json.decode(res.body);
-    if(body['success']){
+    print(body);
+    if (res["message"] != "success") {
+      print("on clicked");
       SharedPreferences localStorage = await SharedPreferences.getInstance();
-      localStorage.remove('user');
-      localStorage.remove('token');
-      Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context)=>Login()));
+      //  localStorage.remove('data');
+      //localStorage.remove('token');
+      localStorage.remove(token);
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
     }
   }
 }
